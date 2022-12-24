683d6c4e3408ab6f1cc03db93e83b80e62b1dedf
tienok2004 <68675946+tienok2004@users.noreply.github.com>
2022-12-24 22:16:38 +0700

    add hentaivn
    add hentaivn

---
+++ hentaivnvip/src/home.js
@@ -0,0 +1,8 @@
+function execute() {
+    return Response.success([
+        {title: "Cập Nhật", input: "https://hentaivnhot.net/truyen-hentai-moi", script: "gen.js"},
+        {title: "Hot Tuần", input: "https://hentaivnhot.net/truyen-hot", script: "gen.js"},
+        {title: "Full Color", input: "https://hentaivnhot.net/the-loai/full-color", script: "gen.js"},
+        {title: "Không Che", input: "https://hentaivnhot.net/the-loai/khong-che", script: "gen.js"},
+    ]);
+}
