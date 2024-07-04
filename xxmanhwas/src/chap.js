3846bb50b222cc521e3138fc216895408dd4d310
tienok2004 <68675946+tienok2004@users.noreply.github.com>
2024-07-05 00:21:28 +0700

    update xxmanhwas

--- xxmanhwas/src/chap.js
+++ xxmanhwas/src/chap.js
@@ -1,39 +1,39 @@
 function execute(url){
-    let response = fetch(url).text()
-    let doc = Html.parse(response)
-    // let loginRequired = doc.select('.story_view_permisstion p.yellowcolor').first()
-    // if (loginRequired != null) {
-    //     return Response.error(`${loginRequired.text()}. Hãy đăng nhập trong trình duyệt Vbook.`)
-    // }
-    let ssrc = doc.select('.cur p img').first().attr('data-src')
-    let sid = response.match(/sid:(\d+)/)[1]
-    let cid = response.match(/cid:(\d+)/)[1]
-    let expiry = response.match(/expire:(\d+)/)[1]
-    let token = response.match(/token:"([0-9a-f.]+)/)[1]
-    let ebe_cap = response.match(/action_ebe_captcha\('([^']+)'\)/)[1]
+    let doc = fetch(url).html()
+    Console.log(doc)
+    let ssrc = doc.select('.cur p img').first().attr('data-src') 
+    let sid = /sid:(\d+)/.exec(doc)[1]
+    let cid = /cid:(\d+)/.exec(doc)[1]
+    let expire = /expire:(\d+)/.exec(doc)[1]
+    let token = /token:"([0-9a-f.]+)/.exec(doc)[1]
+    let ebe_cap = /action_ebe_captcha\('([^']+)'\)/.exec(doc)[1]
     let bodyform = {
         "iid" : `_0_${generateID()}`,
         "ipoi" :  "1",
         "sid" : sid,
         "cid" : cid,
-        "expiry" : expiry,
+        "expire" : expire,
         "token" : token,
         "src" : ssrc,
     }
-    const res = fetch(`https://xxmanhwas.net/${ebe_cap}?_wpnonce=e732af2390628a21d8b7500e621b1493c28d9330b415e88f27b8b4e2f9a440a3`, {
-        method: 'POST',
-        headers : {
-            'referer': url,
-            'x-requested-with': 'XMLHttpRequest'
-        },
-        body: {'nse': Math.random().toString()}
-    }).html();
-    let el = res.select('input')
-    el.forEach(item =>{
-        let name = item.attr('name');
-        bodyform[name] = item.attr('value');
-    })
-    bodyform['doing_ajax'] = 1;
+    let checkLogin = doc.select('.top-login-menu').text();
+    if (checkLogin.indexOf('Đăng xuất') === -1){
+        Console.log('Chưa đăng nhập')
+        const res = fetch(`https://xxmanhwas.net/${ebe_cap}?_wpnonce=e732af2390628a21d8b7500e621b1493c28d9330b415e88f27b8b4e2f9a440a3`, {
+            method: 'POST',
+            headers : {
+                'referer': url,
+                'x-requested-with': 'XMLHttpRequest'
+            },
+            body: {'nse': Math.random().toString()}
+        }).html();
+        let el = res.select('input')
+        el.forEach(item =>{
+            let name = item.attr('name');
+            bodyform[name] = item.attr('value');
+        })
+        bodyform['doing_ajax'] = 1;
+    }
     let json = fetch(`https://xxmanhwas.net/chaps/img`,{
         method : 'POST',
         headers : {
@@ -42,14 +42,18 @@ function execute(url){
         },
         body : bodyform
     }).json();
-    let iUrl = `https://${json.media}/${json.src.split('/O')[0]}/`
+    let isFree = json.is_free
+    let iUrl = `https://${json.media}/${json.src.split('/O')[0]}`
     let imgs = [];
     doc.select('.cur p img').forEach(it =>{
-        imgs.push(iUrl+it.attr('data-src'))
+        var img = '';
+        if(isFree === 1){
+            var img = "/" + it.attr('data-src')
+        }
+        imgs.push(iUrl + img)
     })
     return Response.success(imgs)
 }
-
 function generateID() {
     const chars = '234567abcdefghijklmnopqrstuvwxyz';
     let result = '';
