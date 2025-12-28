43427a6e6d082806bef35dacc311ca017613cfe2
tienok2004 <68675946+tienok2004@users.noreply.github.com>
2025-12-28 17:01:14 +0700

    update trang home

--- vip-hentai/src/home.js
+++ vip-hentai/src/home.js
@@ -1,8 +1,6 @@
 function execute() {
     return Response.success([
         {title: "Cập Nhật", input: "-updated_at", script: "gen.js"},
-        {title: "Mới Nhất", input: "-created_at", script: "gen.js"},
-        {title: "Xem Nhiều", input: "-views", script: "gen.js"},
-        {title: "Hoàn Thành", input: "-updated_at|1", script: "gen.js"},
+        {title: "Manhwa", input: "https://vi-hentai.pro/the-loai/manhwa", script: "cat.js"},
     ]);
 }
