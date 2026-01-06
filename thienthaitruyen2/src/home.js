32e787a6f1225fc2d6b380bca263676f7e55c11c
tienok2004 <68675946+tienok2004@users.noreply.github.com>
2026-01-06 20:08:21 +0700

    update thienthaitruyenv2

--- thienthaitruyen2/src/home.js
+++ thienthaitruyen2/src/home.js
@@ -1,7 +1,7 @@
 load('config.js');
 function execute() {
     return Response.success([
-        {title: "Cập Nhật", input: BASE_URL + "/tim-kiem-nang-cao", script: "cat.js"},
-        {title: "Manhwa", input: BASE_URL + "/the-loai/manhwa", script: "cat.js"},
+        {title: "Cập Nhật", input: BASE_URL + "tim-kiem-nang-cao", script: "cat.js"},
+        {title: "Manhwa", input: BASE_URL + "the-loai/manhwa", script: "cat.js"},
     ]);
 }
